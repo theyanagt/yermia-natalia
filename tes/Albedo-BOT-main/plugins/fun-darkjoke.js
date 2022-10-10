b1925dcae749842ecc9d5b12502f0548eb5cc72c
theyanagt <97217373+theyanagt@users.noreply.github.com>
2022-10-11 05:46:06 +0800

    Delete tes/Albedo-BOT-main directory

--- tes/Albedo-BOT-main/plugins/fun-darkjoke.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn }) => {
-let rest = 'https://api.zacros.my.id/randomimg/darkjokes'
-    conn.sendButtonImg(m.chat, rest, 'Dark? 🤨', wm2, 'NEXT', '.darkjokes', m)
-}
-handler.help = ['darkjokes']
-handler.tags = ['internet', 'fun']
-handler.command = /^(dragjokes|darkjokes)$/i
-
-export default handler
